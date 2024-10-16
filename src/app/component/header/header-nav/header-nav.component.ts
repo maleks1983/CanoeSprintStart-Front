4cabffa70e62aed13fd8f70b106128191c195ad2
maleks1983 <maleks1983@gmail.com>
2024-10-16 13:51:05 +0300

    add header-nav component

--- src/app/component/header/header-nav/header-nav.component.ts
+++ src/app/component/header/header-nav/header-nav.component.ts
@@ -1,12 +1,112 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {NgClass, NgForOf, NgIf, NgOptimizedImage, NgTemplateOutlet} from '@angular/common';
+import { FaIconComponent } from '@fortawesome/angular-fontawesome';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-header-nav',
   standalone: true,
-  imports: [],
+  imports: [
+    NgOptimizedImage,
+    NgForOf,
+    NgIf,
+    NgClass,
+    NgTemplateOutlet,
+    FaIconComponent
+  ],
   templateUrl: './header-nav.component.html',
   styleUrl: './header-nav.component.css'
 })
 export class HeaderNavComponent {
 
+  navbar = [
+    {
+      name: "Новини",
+      list: [],
+      href: ""
+    }, {
+      name: "Федерація",
+      list: [
+        {
+          name: "Президент",
+          href: ""
+        }, {
+          name: "Виконком ВОФВБК",
+          href: ""
+        },
+        {
+          name: "Наша гордість",
+          href: ""
+        }, {
+          name: "Документи",
+          href: ""
+        }, {
+          name: "Партнери,спонсори",
+          href: ""
+        }, {
+          name: "Контакти",
+          href: ""
+        }
+      ],
+      href: ""
+    }, {
+      name: "Змагання",
+      list: [
+        {
+          name: "Результати",
+          href: ""
+        }, {
+          name: "Календар змагань",
+          href: ""
+        },
+        {
+          name: "Система відбору",
+          href: ""
+        }, {
+          name: "Правила змагань",
+          href: ""
+        }, {
+          name: "Положення",
+          href: ""
+        }, {
+          name: "Інші документи",
+          href: ""
+        }
+      ],
+      href: ""
+    }, {
+      name: "Збірна",
+      list: [
+        {
+          name: "Кращі спортсмени",
+          href: ""
+        }, {
+          name: "Склад збірної",
+          href: ""
+        },
+        {
+          name: "Рейтинг спортсменів",
+          href: ""
+        },
+        {
+          name: "Тренери",
+          href: ""
+        }
+      ],
+      href: ""
+    }, {
+      name: "Школи",
+      list: [],
+      href: ""
+    }, {
+      name: "Галерея",
+      list: [],
+      href: ""
+    }, {
+      name: "Антидопінг",
+      list: [],
+      href: ""
+    }
+  ]
+  faSearch = faSearch;
 }
